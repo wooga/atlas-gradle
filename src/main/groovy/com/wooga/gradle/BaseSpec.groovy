7d4999d0fc092bceb1f0d016783baf9d044d786e
Christian Sagel <ckpsm@live.com>
2021-11-02 11:05:07 +0100

    Add a BaseSpec trait, which was found to be common in downstream projects

---
+++ src/main/groovy/com/wooga/gradle/BaseSpec.groovy
@@ -0,0 +1,32 @@
+package com.wooga.gradle
+
+import org.gradle.api.file.ProjectLayout
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.ProviderFactory
+import org.gradle.internal.impldep.org.apache.http.annotation.Obsolete
+
+import javax.inject.Inject
+
+/**
+ * A trait with access to the factories provided by a {@link org.gradle.api.Project}
+ */
+trait BaseSpec {
+    @Inject
+    ProjectLayout getLayout() {
+        throw new Exception("ProjectLayout is supposed to be injected here by gradle")
+    }
+
+    ProviderFactory getProviderFactory() {
+        getProviders()
+    }
+
+    @Inject
+    ProviderFactory getProviders() {
+        throw new Exception("ProviderFactory is supposed to be injected here by gradle")
+    }
+
+    @Inject
+    ObjectFactory getObjects() {
+        throw new Exception("ObjectFactory is supposed to be injected here by gradle")
+    }
+}
